4209ed9a6ca2ce2791d6dbe21b10a7b94077d0b9
Luke Davis <newanswertech@gmail.com>
2021-03-04 05:23:06 -0500

    Basic file setup

--- buildVars.py
+++ buildVars.py
@@ -16,31 +16,31 @@ def _(arg):
 # Add-on information variables
 addon_info = {
 	# add-on Name/identifier, internal for NVDA
-	"addon_name": "addonTemplate",
+	"addon_name": "importJawsDict",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on
 	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary": _("Add-on user visible name"),
+	"addon_summary": _("Import Jaws Dictionary"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""Description for the add-on.
-It can span multiple lines."""),
+	"addon_description": _("""Reads Jaws dictionary files, and adds their entries to one of the NVDA speech dictionaries.
+Start by selecting the "Import Jaws dictionary" entry from the NVDA Tools menu."""),
 	# version
-	"addon_version": "x.y",
+	"addon_version": "0.1-dev",
 	# Author(s)
-	"addon_author": "name <name@domain.com>",
+	"addon_author": "Luke Davis <luke@newanswertech.com>, Brian Vogel <britechguy@gmail.com>",
 	# URL for the add-on documentation support
 	"addon_url": None,
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": None,
+	"addon_minimumNVDAVersion": "2019.3",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": None,
+	"addon_lastTestedNVDAVersion": "2020.4",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
-	"addon_updateChannel": None,
+	"addon_updateChannel": "dev",
 }
 
 # Define the python files that are the sources of your add-on.
@@ -51,7 +51,10 @@ It can span multiple lines."""),
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = []
+pythonSources = [
+	os.path.join("addon", "*.py"),
+	os.path.join("addon", "globalPlugins", "*.py")
+]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
@@ -64,10 +67,3 @@ excludedFiles = []
 # If your add-on is written in a language other than english, modify this variable.
 # For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
 baseLanguage = "en"
-
-# Markdown extensions for add-on documentation
-# Most add-ons do not require additional Markdown extensions.
-# If you need to add support for markup such as tables, fill out the below list.
-# Extensions string must be of the form "markdown.extensions.extensionName"
-# e.g. "markdown.extensions.tables" to add tables.
-markdownExtensions = []
