9205f211b418f5ebea78f50b409434460b77040e
Luke Davis <newanswertech@gmail.com>
2021-03-06 05:32:25 -0500

    BuildVars from another add-on test.

--- buildVars.py
+++ buildVars.py
@@ -3,32 +3,25 @@
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
-
-# Since some strings in `addon_info` are translatable,
-# we need to include them in the .po files.
-# Gettext recognizes only strings given as parameters to the `_` function.
-# To avoid initializing translations in this module we simply roll our own "fake" `_` function
-# which returns whatever is given to it as an argument.
-def _(arg):
-	return arg
-
+# Full getext (please don't change)
+_ = lambda x: x
 
 # Add-on information variables
 addon_info = {
-	# add-on Name/identifier, internal for NVDA
-	"addon_name": "importJawsDict",
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guidelines.txt
+	# add-on Name, internal for nvda
+	"addon_name": "tadd-on",
 	# Add-on summary, usually the user visible name of the addon.
-	# Translators: Summary for this add-on
-	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary": _("Import Jaws Dictionary"),
+	# Translators: Summary for this add-on to be shown on installation and add-on information.
+	"addon_summary": _("test add-on"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""Reads Jaws dictionary files, and adds their entries to one of the NVDA speech dictionaries.
-Start by selecting the "Import Jaws dictionary" entry from the NVDA Tools menu."""),
+	"addon_description": _("""Does very little."""),
 	# version
-	"addon_version": "0.5-dev2",
+	"addon_version": "1.3",
 	# Author(s)
-	"addon_author": "Luke Davis <luke@newanswertech.com>, Brian Vogel <britechguy@gmail.com>",
+	"addon_author": u"Luke Davis (Open Source Systems, Ltd.) <newanswertech@gmail.com>",
 	# URL for the add-on documentation support
 	"addon_url": None,
 	# Documentation file name
@@ -36,23 +29,18 @@ Start by selecting the "Import Jaws dictionary" entry from the NVDA Tools menu."
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2019.3",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2021.3",
-	# Add-on update channel (default is None, denoting stable releases,
-	# and for development releases, use "dev".)
-	# Do not change unless you know what you are doing!
+	"addon_lastTestedNVDAVersion": "2020.3",
+	# Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
 	"addon_updateChannel": None,
 }
 
+
+import os.path
+
 # Define the python files that are the sources of your add-on.
-# You can either list every file (using ""/") as a path separator,
-# or use glob expressions.
-# For example to include all files with a ".py" extension from the "globalPlugins" dir of your add-on
-# the list can be written as follows:
-# pythonSources = ["addon/globalPlugins/*.py"]
-# For more information on SCons Glob expressions please take a look at:
-# https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = ["addon/globalPlugin/importJawsDict.py"]
-#pythonSources = ["addon/*.py", "addon/globalPlugins/*.py"]
+# You can use glob expressions here, they will be expanded.
+pythonSources = [os.path.join("addon", "*.py"),
+os.path.join("addon", "globalPlugins", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
@@ -60,15 +48,3 @@ i18nSources = pythonSources + ["buildVars.py"]
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
 excludedFiles = []
-
-# Base language for the NVDA add-on
-# If your add-on is written in a language other than english, modify this variable.
-# For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
-baseLanguage = "en"
-
-# Markdown extensions for add-on documentation
-# Most add-ons do not require additional Markdown extensions.
-# If you need to add support for markup such as tables, fill out the below list.
-# Extensions string must be of the form "markdown.extensions.extensionName"
-# e.g. "markdown.extensions.tables" to add tables.
-markdownExtensions = []
